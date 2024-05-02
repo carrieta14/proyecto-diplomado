c3154b16a891c019145b98720c529d4351ddeef9
carrieta14 <carrieta14@cuc.edu.co>
2024-05-01 23:04:41 -0500

    book controller

--- src/modules/books/books.controller.ts
+++ src/modules/books/books.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpStatus, Res } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
@@ -8,16 +8,20 @@ export class BooksController {
   constructor(private readonly booksService: BooksService) {}
 
   @Post('/create')
-  create(@Body() createBookDto: CreateBookDto) {
-    return this.booksService.createBook(createBookDto);
+  create(@Body() createBookDto: CreateBookDto, @Res() response) {
+    return this.booksService.createBook(createBookDto).then((book) => {
+      response.status(HttpStatus.CREATED).json({data: book, code: 201, message: 'Libro creado con exito'});
+  }).catch((error)=> {
+      response.status(HttpStatus.BAD_REQUEST).json({message: error.message, code: '400'});
+  });
   }
 
-  @Get()
+  @Get('/show')
   findAll() {
     return this.booksService.findAll();
   }
 
-  @Get(':id')
+  @Get('detail/:id')
   findOne(@Param('id') id: string) {
     return this.booksService.findOne(id);
   }
