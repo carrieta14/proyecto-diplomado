598b913ad00267f1fac6517772fe5b6a1b32045e
Danny <114019450+Dayer2308@users.noreply.github.com>
2024-05-02 21:31:24 -0500

    V8

--- src/modules/books/books.controller.ts
+++ src/modules/books/books.controller.ts
@@ -1,10 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, HttpStatus, Res, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpStatus, Res, UseGuards, Query, } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
 import { AuthGuard } from '@nestjs/passport/dist/auth.guard';
 import { jwtProfileGuard } from '../auth/guard/jwt-profile.guard';
 import { Profiles } from '../auth/decorators/profile.decorator';
+import { Auth } from '../auth/entities/auth.entity';
+import { Book } from './entities/book.entity';
+import { response } from 'express';
+
 
 @Controller('books')
 export class BooksController {
@@ -21,20 +25,68 @@ export class BooksController {
   });
   }
 
+  @UseGuards(AuthGuard())
   @Get('/show')
-  findAll() {
-    return this.booksService.findAll();
+  findAll(@Res() response) {
+    return this.booksService.findAll().then((books) => {
+      response.status(HttpStatus.CREATED).json({data: books, code: 201, message: 'Libros encontrados'});
+  }).catch((error)=> {
+      response.status(HttpStatus.BAD_REQUEST).json({message: error.message, code: '400'});
+    })
   }
 
-  @Get('detail/:id')
-  findOne(@Param('id') id: string) {
-    return this.booksService.findOne(id);
+  @UseGuards(AuthGuard())
+  @Get('/detail/:id')
+  findOne(@Query('id') id: string, @Res() response) {
+    return this.booksService.findOne(id).then((book) => {
+      response.status(HttpStatus.CREATED).json({data: book, code: 201, message: 'Libro encontrado'});
+  }).catch((error)=> {
+      response.status(HttpStatus.BAD_REQUEST).json({message: error.message, code: '400'});
+    });
   }
+
+
+  @UseGuards(AuthGuard())
+  @Post('/addFavorite/:id')
+  addFavorite(@Res() response, @Query('userId') userId: string, @Query('bookId') bookId: string) {
+    return this.booksService.addFavorite(userId, bookId).then((addFavorite) => {
+      response.status(HttpStatus.CREATED).json({ data: addFavorite, code: 201, message: 'Libro asignado en favorito' });
+    }).catch((error) => {
+      response.status(HttpStatus.BAD_REQUEST).json({ message: error.message, code: '400' });
+    });
+  }
+
+  @UseGuards(AuthGuard())
+  @Delete('/removeFavorite/:id')
+  removeFavorite(@Res() response, @Query('user') user: Auth, @Query('book') book: Book) {
+    return this.booksService.removeFavorite(user, book).then((addFavorite) => {
+      response.status(HttpStatus.CREATED).json({ data: addFavorite, code: 201, message: 'Libro Eliminado en favorito' });
+    }).catch((error) => {
+      response.status(HttpStatus.BAD_REQUEST).json({ message: error.message, code: '400' });
+    });
+  }
+
+  @UseGuards(AuthGuard())
+  @Get('/getUserFavorites/')
+  getUserFavorites(@Res() response, @Query('user') user: Auth) {
+    return this.booksService.getUserFavorites(user).then((addFavorite) => {
+      response.status(HttpStatus.CREATED).json({ data: addFavorite, code: 201, message: 'Lista de libros favoritos' });
+    }).catch((error) => {
+      response.status(HttpStatus.BAD_REQUEST).json({ message: error.message, code: '400' });
+    });
+  }
+
 
   @UseGuards(AuthGuard(),jwtProfileGuard)
   @Profiles(1001)
-  @Patch('/update_book/:id/:rol')
-  update(@Param('id') id: string, @Body() updateBookDto: UpdateBookDto) {
-    return this.booksService.updateBook(id, updateBookDto);
+  @Patch('/update_book/:id')
+  update(@Query('id') id: string, @Body() updateBookDto: UpdateBookDto, @Res() response) {
+    return this.booksService.updateBook(id, updateBookDto).then((book) => {
+      response.status(HttpStatus.CREATED).json({ data: book, code: 201, message: 'Libro Eliminado en favorito' });
+    }).catch((error) => {
+      response.status(HttpStatus.BAD_REQUEST).json({ message: error.message, code: '400' });
+    });
   }
+
+
 }
